96e102e047f3984d03962f9d760a1e7e326d040c
Ainhoa <ainhoa.urtasun@unavarra.es>
2022-10-11 21:27:35 +0200

    Update Client.py

--- Client.py
+++ Client.py
@@ -5,7 +5,8 @@ from diagrams.aws.network import Route53
 from diagrams.onprem.database import PostgreSQL # Would typically use RDS from aws.database
 from diagrams.onprem.inmemory import Redis # Would typically use ElastiCache from aws.database
 
-with Diagram("Simple Website Diagram") as diag:
+def Client():
+  with Diagram("Simple Website Diagram") as diag:
     dns = Route53("dns")
     load_balancer = ELB("Load Balancer")
     database = PostgreSQL("User Database")
@@ -13,4 +14,3 @@ with Diagram("Simple Website Diagram") as diag:
     svc_group = [EC2("Webserver 1"),
                  EC2("Webserver 2"),
                  EC2("Webserver 3")]
-diag # This will illustrate the diagram if you are using a Google Colab or Jypiter notebook.
