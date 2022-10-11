45f04407e108cc2201cb238e110670d2ef8a26d1
Ainhoa <ainhoa.urtasun@unavarra.es>
2022-10-11 23:01:39 +0200

    Update Client.py

--- Client.py
+++ Client.py
@@ -4,3 +4,20 @@ G.node('Support',r'Finance\nAccounting\nManagement')
 def Core(label):
   G.node('Core',label)
 G.edge('Support','Core')
+
+
+from diagrams import Diagram, Cluster
+from diagrams.aws.compute import EC2
+from diagrams.aws.network import ELB
+from diagrams.aws.network import Route53
+from diagrams.onprem.database import PostgreSQL # Would typically use RDS from aws.database
+from diagrams.onprem.inmemory import Redis # Would typically use ElastiCache from aws.database
+
+with Diagram("Simple Website Diagram") as diag:
+    dns = Route53("dns")
+    load_balancer = ELB("Load Balancer")
+    database = PostgreSQL("User Database")
+    cache = Redis("Cache")
+    svc_group = [EC2("Webserver 1"),
+                 EC2("Webserver 2"),
+                 EC2("Webserver 3")]
