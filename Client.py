d5f6b6d14b010cf46d95b9bbd8d28ce44866c39b
Ainhoa <ainhoa.urtasun@unavarra.es>
2022-10-11 23:30:42 +0200

    Update Client.py

--- Client.py
+++ Client.py
@@ -3,7 +3,6 @@ from diagrams.aws.compute import EC2
 from diagrams.aws.database import RDS
 from diagrams.aws.network import ELB
 
-with Diagram('Client', show=False) as flow:
+def label(l):
+  with Diagram(l) as flow:
     ELB("Support") >> EC2("Core")
-def explain(label):
-    print(label)
