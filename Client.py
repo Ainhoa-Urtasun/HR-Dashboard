6ea48276af01bc5c991392dab2fdb85891fe6eea
Ainhoa <ainhoa.urtasun@unavarra.es>
2022-10-11 22:31:10 +0200

    Update Client.py

--- Client.py
+++ Client.py
@@ -1,3 +1,6 @@
-def printinfo(name,age):
-   print("Name: ", name)
-   print("Age ", age)
+from graphviz import Digraph
+G = Digraph()
+G.node('Support',r'Finance\nAccounting\nManagement')
+def Core(label):
+  G.node('Core',label)
+G.edge('Support','Primary')
