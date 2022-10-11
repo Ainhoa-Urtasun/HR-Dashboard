6b2ad34ce92e4110fcaf6eedd0ee472c310cbe6a
Ainhoa <ainhoa.urtasun@unavarra.es>
2022-10-11 20:25:47 +0200

    Update Client.py

--- Client.py
+++ Client.py
@@ -1,7 +1,5 @@
-def Client(Core):
-  labels = {0:Core,1:'SUPPORT ACTIVITIES\nManagement\nHRM\nFinance\nAccounting'
-  colors = {0:'grey',1:'gold'}
-  shapes = {0:'o',1:'o'}
-  client = [(1,0)]
-  fig = plt.figure(figsize=(5,5))
+from diagrams import Diagram
+with Diagram('Simple Website Diagram') as diag:
+  pass
+diag
 
