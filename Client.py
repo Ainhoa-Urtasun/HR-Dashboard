27d0d2b86baa91fbccd7ee632bd72b6d0d9e1896
Ainhoa <ainhoa.urtasun@unavarra.es>
2022-10-11 22:01:31 +0200

    Update Client.py

--- Client.py
+++ Client.py
@@ -1,6 +1,6 @@
 from graphviz import Digraph
-def Client():
-  G = Digraph(description)
+def Client(description):
+  G = Digraph()
   G.node('Support',r'Finance\nAccounting\nManagement')
   G.node('Core',description)
   G.edge('Support','Primary')
