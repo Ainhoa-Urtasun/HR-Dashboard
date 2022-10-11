78b6b52c76e5341553e122bb28345fd80457dbda
Ainhoa <ainhoa.urtasun@unavarra.es>
2022-10-11 23:23:02 +0200

    Update Client.py

--- Client.py
+++ Client.py
@@ -1,15 +1,7 @@
-from graphviz import Digraph
-G = Digraph()
-G.node('Support',r'Finance\nAccounting\nManagement')
-def Core(label):
-  G.node('Core',label)
-G.edge('Support','Core')
-
-
 from diagrams import Diagram
 from diagrams.aws.compute import EC2
 from diagrams.aws.database import RDS
 from diagrams.aws.network import ELB
 
-with Diagram("Making shoes", show=False) as flow:
+with Diagram(label, show=False) as flow:
     ELB("Support") >> EC2("Core")
