8242b5f16cbba5fb44e2714c5d04b5b85629a88f
Ainhoa <ainhoa.urtasun@unavarra.es>
2022-10-11 20:21:51 +0200

    Update Client.py

--- Client.py
+++ Client.py
@@ -1,10 +1,9 @@
-import matplotlib.pyplot as plt
-from netgraph import Graph
+pip install diagrams
 
 def Client(Core):
-  labels = {0:Core,1:'SUPPORT ACTIVITIES\nManagement\nHRM\nFinance\nAccounting'}
+  labels = {0:Core,1:'SUPPORT ACTIVITIES\nManagement\nHRM\nFinance\nAccounting'
   colors = {0:'grey',1:'gold'}
   shapes = {0:'o',1:'o'}
   client = [(1,0)]
   fig = plt.figure(figsize=(5,5))
-  Graph(client,node_size=30,node_labels=labels,node_color=colors,node_shape=shapes,node_edge_width=2,edge_width=2,arrows=True)
+
